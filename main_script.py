2ea7256bf1ac4013cfe7ef47c4610b6523e44271
tallkyy <timolassoeur@eduroamstud-10-255-233-173.unisg.ch>
2026-03-09 19:03:01 +0100

    update project

--- main_script.py
+++ main_script.py
@@ -3,3 +3,5 @@ print('Je suis Teva et je viens de push cette ligne de code sur GitHub')
 print('Je suis Morgane et je viens de push cette ligne de code sur GitHub')
 
 print ("Je suis Charlotte et je viens de push cette ligne de code sur GitHub")
+
+print("Je suis Timo et je ne viens pas de push cette ligne de code sur Github")
