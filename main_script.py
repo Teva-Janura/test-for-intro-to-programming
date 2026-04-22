a230ed21bf30e4a7fd8bf16c640545ab035ceedc
Teva-Janura <janurateva@icloud.com>
2026-04-22 11:48:27 +0200

    new change

--- main_script.py
+++ main_script.py
@@ -7,3 +7,5 @@ print ("Je suis Charlotte et je viens de push cette ligne de code sur GitHub")
 print("Je suis Timo et je ne viens pas de push cette ligne de code sur Github")
 
 print("Je suis Satine et je viens de push cette ligne de code sur GitHub")
+
+print("new change")
