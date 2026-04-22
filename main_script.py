ebf26e07a23616763ff27a52d9dec0d457d80f79
Teva-Janura <janurateva@icloud.com>
2026-04-22 11:45:32 +0200

    xxx

--- main_script.py
+++ main_script.py
@@ -4,4 +4,6 @@ print('Je suis Morgane et je viens de push cette ligne de code sur GitHub')
 
 print ("Je suis Charlotte et je viens de push cette ligne de code sur GitHub")
 
-print("Je suis Timo et je ne viens pas de push cette ligne de code sur Github")print("Je suis Satine et je viens de push cette ligne de code sur GitHub")
+print("Je suis Timo et je ne viens pas de push cette ligne de code sur Github")
+
+print("Je suis Satine et je viens de push cette ligne de code sur GitHub")
